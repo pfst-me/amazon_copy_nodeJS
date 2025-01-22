64b4e106f275b2897b5ea53cc60d588ccc81631e
naikpavan56 <naikpavan56@gmail.com>
2025-01-22 15:32:13 +0530

    update product logic

--- controller/productController.js
+++ controller/productController.js
@@ -1,9 +1,28 @@
 const products = require('../models/products');
+const mongoose = require('mongoose');
 
 exports.createOrUpdateProducts = async (req, res) => {
   try {
     const { _id, ...productsData } = req.body;
-    const query = _id ? { _id: mongoose.Types.ObjectId(_id) } : {};
+    let query = {};
+    
+    if(_id) {
+      if(!mongoose.isValidObjectId(_id)){
+        return res.status(400).json({
+          success: false,
+          message: 'invalid _id provided'
+        })
+      }
+      query = { _id: new mongoose.Types.ObjectId(_id) };
+    } else {
+      const newProduct = new products(productsData)
+      await newProduct.save(newProduct)
+
+      return res.status(201).json({
+        success: true,
+        message: 'Producted created successfully.'
+      })
+    }
 
     await products.findOneAndUpdate(query, productsData, {
       new: true,
@@ -11,14 +30,17 @@ exports.createOrUpdateProducts = async (req, res) => {
       setDefaultsOnInsert: true,
     });
 
-    res
-      .status(201)
-      .json({ success: true, message: 'Producted added successfully.' });
+    res.status(201).json({
+      success: true,
+      message: 'Producted updated successfully.'
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Network error on updating products.' });
   }
 };
+
+
 
 exports.getProducts = async (req, res) => {
   try {
